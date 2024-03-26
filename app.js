cb0ac446121c4a2bef19065dda07f15a2ed9927c
Tushar007079 <tusharpanchal21@gnu.ac.in>
2024-03-26 19:46:53 +0530

    This is practical9 v2.0

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const port = process.env.PORT || 8080;
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => {
+  console.log(`API server listening on port ${port}`);
+});
